bc40ea9ec019acc3b3e277e621114acb5c15af0f
KaushalKrSoni <kaushal.ece21@gmail.com>
2024-05-15 07:34:29 +0530

    done

---
+++ Day09/D9_A1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/types.h>
+
+int main()
+{
+	int ret, signum, pid;
+
+	printf("Enter Signal Number: ");
+	scanf("%d", &signum);
+
+	printf("Enter Process I'd: ");
+	scanf("%d", &pid);
+
+	ret = kill(pid, signum);
+	if(ret == 0)
+		printf("Signal: %d, send to Process: %d ", signum, pid);
+
+	else
+		printf("failed to send signal(%d) to process(%d)",signum, pid);
+
+	return 0;
+}
