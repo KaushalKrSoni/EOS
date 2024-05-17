e013fa46ea8544a13b3a9087c611ae5fd2c4f95f
KaushalKrSoni <kaushal.ece21@gmail.com>
2024-05-17 18:53:04 +0530

    Assign1

---
+++ Day12/D12_A1_rd.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// p2 -- reader
+int main() {
+	int Fd,fd,a,b,x,y,sum;
+	fd = open("fifo1", O_RDONLY);
+	if(fd < 0) {
+		perror("open() failed");
+		_exit(1);
+	}
+
+	printf("waiting for data...\n");
+	a = read(fd, &x, sizeof(x));
+	b = read(fd, &y, sizeof(y));
+	printf("read from fifo1: %d and %d\n", x,y);
+	sum = x + y;
+		
+	Fd = open("fifo2", O_WRONLY);
+	if(Fd < 0) {
+		perror("open() failed");
+		_exit(1);
+	}
+	write(Fd, &sum, sizeof(sum));
+	printf("sum sent to client by another fifo: \n");
+	close(fd);
+	return 0;
+}
+
+
